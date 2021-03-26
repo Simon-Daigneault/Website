07e2ec934273be8bfc60a422daec94b6a972d082
muliPuli <milan.paczai20@imperial.ac.uk>
2021-03-26 13:07:00 +0100

    added fonts, basic structure, threejs, style
    Set up the basic files

---
+++ javascript.js
@@ -0,0 +1,23 @@
+const landingPage = document.getElementsByClassName("landingPage")[0];
+
+const mainTitle = landingPage.querySelector("h1");
+
+
+mainTitle.style.top = window.innerHeight - 200 + "px";
+
+
+
+
+//landingPage.style.background = "#0d4";
+
+
+
+
+/*landingPage.style.width = window.innerWidth + "px";
+landingPage.style.height = window.innerHeight + "px";
+*/
+window.addEventListener("resize", () => {
+    mainTitle.style.top = window.innerHeight - 200 + "px";
+})
+
+
