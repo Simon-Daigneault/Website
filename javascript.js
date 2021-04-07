825d3fd889027b16f53cb15e8156b6b30588b62e
muliPuli <milan.paczai20@imperial.ac.uk>
2021-04-07 18:30:57 +0200

    leads section updated

--- javascript.js
+++ javascript.js
@@ -2,6 +2,32 @@ const landingPage = document.getElementsByClassName("landingPage")[0];
 
 const mainTitle = landingPage.querySelector("h1");
 
+const leadsTitlesContainer = document.getElementById("team-name");
+const leadsTitles = document.getElementById("team-name").childNodes;
+
+
+
+const leadsDescriptionsContainer = document.getElementById("team-description");
+
+const leadsDescriptions = document.getElementById("team-description").childNodes;
+
+for (let i = 0; i < leadsTitles.length; i++) {
+
+    if(i % 2 == 1){
+        leadsTitles[i].style.height = leadsTitlesContainer.clientHeight + "px";
+    }
+
+}
+
+
+for (let i = 0; i < leadsDescriptions.length; i++) {
+
+    if(i % 2 == 1){
+        leadsDescriptions[i].style.height = leadsDescriptionsContainer.clientHeight + "px";
+    }
+
+}
+
 
 mainTitle.style.top = window.innerHeight - 200 + "px";
 landingPage.style.height = window.innerHeight + "px";
