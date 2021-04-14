621507d3f5451a8721539f9d5fd8d43273cb2d4f
LegoYoda112 <thomas.godden@outlook.com>
2021-04-14 16:36:49 +0100

    Basic pod 3D model

--- 3dpod/scene/pod.js
+++ 3dpod/scene/pod.js
@@ -1,6 +1,14 @@
 import { STLLoader } from "/three/examples/jsm/loaders/STLLoader.js";
+import { Mesh, 
+    MeshNormalMaterial,
+    MeshStandardMaterial } from 'https://unpkg.com/three@0.117.0/build/three.module.js'
 
 import { setupModel } from './setupModel.js';
+
+var material = new MeshStandardMaterial({metalness: 0, 
+    roughness: 0.5,
+    color: 0xFFA500});
+
 
 async function loadPod() {
     const loader = new STLLoader();
@@ -9,9 +17,9 @@ async function loadPod() {
 
     console.log("Zooom", podData);
 
-    const pod = setupModel(podData);
+    const pod_mesh = new Mesh(podData, material);
 
-    return { pod };
+    return pod_mesh;
 }
 
 export { loadPod };
--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 import { World } from '/3dpod/World.js';
 
 async function main() {
-    const container = document.getElementById("3dpod-container");
+    const container = document.getElementById("pod-container");
 
     const world = new World(container);
 
     await world.init();
+
+    world.start();
 }
 
 main();
